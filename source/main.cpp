967b48e43ad27199ee61820eeeb9f2d1eb7b154f
rafal <kipu44@wp.pl>
2019-06-09 00:54:34 +0200

    [WIP] perfect forwading.

--- source/main.cpp
+++ source/main.cpp
@@ -3,15 +3,62 @@
 #include "indent.h"
 #include "raii.h"
 
+#include <memory>
+
+// template <typename T>
+// auto makeUnique(T && t)
+// {
+//     const Raii r0{0};
+//     return std::make_unique<T>(std::forward<T>(t));
+// }
+
+// template <typename T, typename A>
+// auto makeUnique(A && a)
+// {
+//     const Raii r0{0};
+//     return std::make_unique<T>(std::forward<A>(a));
+// }
+
+// template<typename T, typename ... Args>
+// auto makeUnique(Args && ... args)
+// {
+//     const Raii r0{0};
+//     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+// }
+
+// #define makeUnique std::make_unique
+
+template<typename T, typename ... Args>
+auto makeUnique(Args && ... args)
+{
+    const Raii r0{0};
+    return std::unique_ptr<T>(new T(args...));
+}
+
 int main()
 {
-    Indent::i = 2;
-    Indent::n = 0;
+    {
+        const Raii r1{9};
 
-    const Raii r;
+        Bar b1(Foo{});
+        const Bar b2(std::move(b1));
+    }
+    {
+        const Raii r1{1};
 
-    Bar b1(Foo{});
-    const Bar b2(std::move(b1));
+        const auto b1 = makeUnique<Bar>(Foo{});
+    }
+    {
+        const Raii r2{2};
 
+        Foo f2;
+        const auto b2 = makeUnique<Bar>(std::move(f2));
+    }
+    {
+        const Raii r3{3};
+
+        const Foo f3;
+        const auto b3 = makeUnique<Bar>(f3);
+    }
     return 0;
 }
