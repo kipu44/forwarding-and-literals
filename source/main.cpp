6fdcd99f7b864153bc169761da2ab3f733fb408c
rafal <kipu44@wp.pl>
2019-06-09 16:37:05 +0200

    forwarding 1

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,26 @@
 
 #include "foo-bar.h"
-#include "indent.h"
 #include "raii.h"
+
+#include <memory>
+
+// #define makeUnique std::make_unique
+
+#ifndef makeUnique
+template <typename T>
+auto makeUnique(T arg)
+{
+    const Raii r0{0};
+    return T(arg);
+}
+#endif
 
 int main()
 {
-    Indent::i = 2;
-    Indent::n = 0;
+    {
+        const Raii r1{1};
 
-    const Raii r(9);
-
-    Bar b1(Foo{});
-    const Bar b2(std::move(b1));
-
+        const auto b1 = makeUnique<Bar>(Bar{Foo{}});
+    }
     return 0;
 }
