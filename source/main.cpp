dd29ebae3dcf25cb88448b2c63c81c23c8abb01d
rafal <kipu44@wp.pl>
2019-06-09 16:26:08 +0200

    Introduced Counter, Indent, Raii, Foo and Bar types.

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,17 @@
 
-#include <iostream>
+#include "foo-bar.h"
+#include "indent.h"
+#include "raii.h"
 
 int main()
 {
+    Indent::i = 2;
+    Indent::n = 0;
+
+    const Raii r(9);
+
+    Bar b1(Foo{});
+    const Bar b2(std::move(b1));
+
     return 0;
 }
