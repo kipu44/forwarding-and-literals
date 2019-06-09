c64d10818bb93d7e2634e40f8f7f01aa61f8a850
rafal <kipu44@wp.pl>
2019-06-09 19:00:17 +0200

    solution

--- source/size.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "size.h"
-
-constexpr unsigned int operator ""_elements(const unsigned long long n)
-{
-    return static_cast<int>(n);
-}
--- source/size.h
+++ source/size.h
@@ -2,6 +2,9 @@
 #ifndef __SIZE_H__
 #define __SIZE_H__
 
-constexpr unsigned int operator""_elements(unsigned long long n);
+constexpr unsigned int operator""_elements(const unsigned long long n)
+{
+    return static_cast<unsigned int>(n);
+}
 
 #endif
