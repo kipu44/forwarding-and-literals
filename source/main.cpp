0abc657556e86cb8e4a5915786a28d6adc284b5b
rafal <kipu44@wp.pl>
2019-06-09 18:51:27 +0200

    solution

--- source/main.cpp
+++ source/main.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 #include <vector>
 
-unsigned int operator""elements(const unsigned long long n)
+unsigned int operator""_elements(const unsigned long long n)
 {
     return static_cast<int>(n);
 }
 
 int main()
 {
-    const std::vector<int> a(3elements, 1);
+    const std::vector<int> a(3_elements, 1);
 
     std::cout << "size = " << a.size() << "\n";
     std::cout << "second element = " << a[1u] << "\n";
